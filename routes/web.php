2c9c2b77831a9794cb9d9d5455c2676c6dd70acc
Prateek Kher <prateek.kher@gmail.com>
2021-05-09 19:33:32 +0530

    Added Routes for getting State and District

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Models\District;
+use App\Models\State;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -16,3 +19,27 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group([], function () use ($router) {
+    $router->get('state[/{state_id:[0-9]+}]', function($state_id = NULL) { 
+        if (is_null($state_id)) {
+            return response()->json(State::all()); 
+        }
+        return response()->json(State::findOrFail($state_id));
+    });
+    $router->get('state/{state_id:[0-9]+}/districts', function($state_id) {
+        $district = District::where('state_id', $state_id)->get();
+        return response()->json($district);
+    });
+});
+
+$router->group(['prefix' => 'create'], function () use ($router) {
+    $router->post('state', ['use' => 'StateController@create']);
+    $router->post('district', ['use' => 'DistrictController@create']);
+});
+
+$router->group(['prefix' => 'update'], function () use ($router) {
+    $router->put('state', ['use' => 'StateController@update']);
+    $router->put('district', ['use' => 'DistrictController@update']);
+});
+
